f161e2535d55e2419b5eda54f7330bbb9cd18c2b
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2022-07-05 10:21:32 -0600

    structure initial

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+	return <h1>template mui vite</h1>;
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@assets': path.resolve(__dirname, 'src/assets'),
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@utils': path.resolve(__dirname, 'src/utils')
+		}
+	}
+});
